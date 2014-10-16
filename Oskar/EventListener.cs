7e42f9509c16c641a27cc9c917333bf98cf9c8b8
Peep <peep@bmrf.me>
2014-10-15 23:37:07 -0300

    Remove unused projects

---
+++ Oskar/EventListener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ChatSharp;
+using ChatSharp.Events;
+
+namespace Oskar
+{
+    public class EventListener
+    {
+        private readonly IrcClient _client;
+
+        public EventListener()
+        {
+            _client = Bot.Instance.Client;
+        }
+
+        public void Listen()
+        {
+            _client.RawMessageRecieved += OnRawMessageReceived;
+            _client.RawMessageSent += OnRawMessageSent;
+            _client.ConnectionComplete += OnConnectionComplete;
+        }
+
+        private static void OnRawMessageReceived(object sender, RawMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        private static void OnRawMessageSent(object sender, RawMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        private void OnConnectionComplete(object sender, EventArgs e)
+        {
+            if (Bot.Instance.Config.NickServEnabled)
+                NickServLogin();
+
+            foreach (var channel in Bot.Instance.Config.AutoJoinChannels)
+                _client.JoinChannel(channel);
+        }
+
+        private void NickServLogin()
+        {
+            if (String.Equals(_client.User.Nick, Bot.Instance.Config.NickServUser, StringComparison.CurrentCultureIgnoreCase))
+                _client.SendMessage("IDENTIFY " + Bot.Instance.Config.NickServPass, "NICKSERV");
+        }
+    }
+}
---
+++ TestPlugin/TestClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ChatSharp.Events;
+using Oskar;
+
+namespace TestPlugin
+{
+    public class TestClass : Plugin
+    {
+        public override string Name { get { return "Test Plugin"; }}
+        public override string Author { get { return "Peep"; }}
+
+        public override void OnCreate()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hi. I'm a plugin! I live in " + AppDomain.CurrentDomain.FriendlyName);
+            Console.ResetColor();
+
+            Bot.Instance.Client.UserJoinedChannel += OnUserJoinedChannel;
+        }
+
+        public void OnUserJoinedChannel(object sender, ChannelUserEventArgs e)
+        {
+            Bot.Instance.Client.SendMessage("Sup, " + e.User, e.Channel.Name);
+        }
+
+        public override void OnDestroy()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Bye. I'm a plugin! I live in " + AppDomain.CurrentDomain.FriendlyName);
+            Console.ResetColor();
+        }
+    }
+}
